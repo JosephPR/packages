c99f5ed6e379d171e62fee82afa67d1ab7e26d6c
JosephPR <reilly4181@gmail.com>
2019-10-08 11:13:16 -0600

    icons for links

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Texty from 'rc-texty';
+import {Icon} from 'antd'
 import QRCode from 'qrcode.react';
 import '../App.css'
 
@@ -9,7 +10,20 @@ export default function Home () {
    <Texty mode='random' type='bounce' duration={1000}>Hello</Texty>
    <Texty mode='smooth'  duration={4000}>Welcome to my Portfolio</Texty>
    <Texty mode='sync'  duration={3000}>My name is Joseph Reilly</Texty>
-   <QRCode value='https://github.com/JosephPR' />
+   <div className='qr'>
+     <h3><a className='github' href='https://github.com/JosephPR'><Icon type="github" /></a></h3>
+     <h3><a className='linkedin' href='https://www.linkedin.com/in/josephpr/'><Icon type="linkedin" /></a></h3>
+     <h3><a className='instagram' href='https://www.instagram.com/flipnoggin/'><Icon type="instagram" /></a></h3>
+     <h3><a className='facebook' href='https://github.com/JosephPR'><Icon type="facebook" /></a></h3>
+   </div>
+   <div className='qr'>
+     <QRCode value='https://github.com/JosephPR' />
+     <QRCode value='https://www.linkedin.com/in/josephpr/' />
+     <QRCode value='https://www.instagram.com/flipnoggin/' />
+     <QRCode value='https://github.com/JosephPR' />
+
+   </div>
+
  </div>
 
   )
