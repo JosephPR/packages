28891f8e1cf99dc2db597b091aa0e4757040c332
JosephPR <reilly4181@gmail.com>
2019-10-23 23:14:07 -0600

    86ed

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,7 +7,6 @@ export default function About() {
       <h3 className="about-paragraph">I am a full stack developer that has a passion to create new and innovative technologies.<br/><br/>
         Recently I graduated from the Flatiron school's software immersive bootcamp, I learned so much
         from that experience and gain many new friends along the way. The primary stack is Ruby and Javascript. Everyday is an opportunity to learn something, build upon a solid foundation of programming knowledge.
-      <br/><br/> I'm continuing my journey as a teaching assistant for Trilogy education at Denver University, to help other people learn and grow.
         <br/><br/>My hobbies include snowboarding, hiking, camping, guitar playing, archery, disc golf and of course programming. <br/><br/>
       </h3>
     </div>
